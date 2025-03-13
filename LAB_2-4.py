7c03c4ca9854ffa5631696a9430cdcb0a274d6c5
cga07 <sijrgy11@naver.com>
2025-03-13 12:39:14 +0900

    first commit

---
+++ LAB_2-4.py
@@ -0,0 +1,17 @@
+name = 'si ga yeon'
+print('my name :', name)
+# my name : si ga yeon
+age = 23
+print('my age :', age)
+# my age : 23
+height = 167
+print('my height', height, 'cm')
+# my height 167 cm
+sum = 10 + 20
+print('10 + 20 =', sum)
+# 10 + 20 = 30
+mult = 10 * 20
+print('10 * 20 = ', mult)
+# 10 * 20 = 200
+
+
